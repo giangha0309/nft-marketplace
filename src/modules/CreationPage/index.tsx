5748f3731b7f4dc8cbad1528a8cfd9596b127bbb
Giang <giangtdk0309@gmail.com>
2023-12-27 15:21:52 +0700

    upload NFT metadata and mint first NFT

--- src/modules/CreationPage/index.tsx
+++ src/modules/CreationPage/index.tsx
@@ -1,12 +1,15 @@
 import classNames from "classnames";
 import CreationForm, { CreationValues } from "./CreationForm";
 import useSigner from "state/signer";
+import useNFTMarket from "state/nft-market";
 
 const CreationPage = () => {
   const { signer } = useSigner ();
+  const { createNFT } = useNFTMarket();
 
   const onSubmit = async (values: CreationValues) => {
     // TODO: create NFT
+    console.log(values);
   };
 
   return (
@@ -15,7 +18,7 @@ const CreationPage = () => {
         "items-center justify-center": !signer,
       })}
     >
-      {signer ? <CreationForm onSubmit={onSubmit} /> : "Connect your wallet"}
+      {signer ? <CreationForm onSubmit={createNFT} /> : "Connect your wallet"}
     </div>
   );
 };
